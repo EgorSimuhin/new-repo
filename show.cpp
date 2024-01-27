0c22aff831fea8aaa69090fdc4d5cfc20f056695
Егор Симухин <simuhinegor22@gmail.com>
2024-01-27 21:23:08 +0300

    add show and gitignore

---
+++ show.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "RU");
+    int f, g, nod = 1;
+    cin >> f >> g;
+    for(int u = 1; u <= g; u++) {
+        if(f % u == 0 && g % u == 0) {
+            nod = u;
+        }
+    }
+    cout << "НОД - " << nod;
+    return 0;
+}
